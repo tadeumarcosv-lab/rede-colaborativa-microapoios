ec4c03b3936d43a516214442a0fda1417a6e557a
Tadeu Marcos Viana <tadeumarcosv@gmail.com>
2026-07-13 10:52:34 -0300

    Corrigir import do config_nucleo para usar arquivo da raiz corretamente

--- nucleo_operacional/integracao_completa.py
+++ nucleo_operacional/integracao_completa.py
@@ -4,7 +4,8 @@ from nucleo_operacional.agente_comunicacao import AgenteComunicacao
 from nucleo_operacional.agente_pesquisa_avancada import AgentePesquisaAvancada
 from nucleo_operacional.agente_memoria_estrategica import AgenteMemoriaEstrategica
 from nucleo_operacional.agente_gestao_conhecimento import AgenteGestaoConhecimento
-from nucleo_operacional.config_nucleo import *
+
+from config_nucleo import *
 
 
 class IntegracaoCompleta:
@@ -21,10 +22,15 @@ class IntegracaoCompleta:
     def executar(self, solicitacao):
 
         etapa1 = self.comunicacao.executar(solicitacao)
+
         etapa2 = self.coordenacao.executar(etapa1)
+
         etapa3 = self.central.executar(etapa2)
+
         etapa4 = self.pesquisa.executar(etapa3)
+
         etapa5 = self.memoria.executar(etapa4)
+
         etapa6 = self.conhecimento.executar(etapa5)
 
         return etapa6
